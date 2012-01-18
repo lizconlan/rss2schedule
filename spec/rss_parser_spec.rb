86ff6278ec6c78ad97f20659572a05a90305dcc3
lizconlan <lizconlan@gmail.com>
2012-01-18 15:51:43 +0000

    stubbing the call to RestClient.get

--- spec/rss_parser_spec.rb
+++ spec/rss_parser_spec.rb
@@ -1,27 +1,23 @@
 require 'spec_helper'
  
-describe RSSParser do
+describe RSSParser do 
+  before :each do
+    RestClient.stub(:get).and_return("rss")
+    @rssparser = RSSParser.new
+  end
+  
+  describe "#new" do
+    it "takes no parameters and returns a RSSParser object" do
+      @rssparser.should be_an_instance_of RSSParser
+    end
     
-    before :each do
-	    @rssparser = RSSParser.new
+    it "returns the correct feed url" do
+      @rssparser.feed_url.should eql "http://services.parliament.uk/calendar/all.rss"
     end
-
-    describe "#new" do
-	    it "takes no parameters and returns a RSSParser object" do
-		    @rssparser.should be_an_instance_of RSSParser
-	    end
-	
-	    it "returns the correct feed url" do
-		    @rssparser.feed_url.should eql "http://services.parliament.uk/calendar/all.rss"
-	    end
-
-      it "accepts an alternate feed url" do
-        parser = RSSParser.new("http://example.com/fake.rss")
-        parser.feed_url.should eql "http://example.com/fake.rss"
-      end
-
+    
+    it "accepts an alternate feed url" do
+      parser = RSSParser.new("http://example.com/fake.rss")
+      parser.feed_url.should eql "http://example.com/fake.rss"
     end
-
+  end
 end
-
-
