ac2eaadb4607f6ce1ea0fe4dbff787e0778df55f
Robert Brook <mail@robertbrook.com>
2012-01-19 11:15:57 +0000

    more stubs in specs

--- spec/rss_parser_spec.rb
+++ spec/rss_parser_spec.rb
@@ -6,7 +6,7 @@ describe RSSParser do
     @rssparser = RSSParser.new
   end
   
-  describe "#new" do
+  describe "initialize" do
     it "takes no parameters and returns a RSSParser object" do
       @rssparser.should be_an_instance_of RSSParser
     end
@@ -16,8 +16,22 @@ describe RSSParser do
     end
     
     it "accepts an alternate feed url" do
-      parser = RSSParser.new("http://example.com/fake.rss")
-      parser.feed_url.should eql "http://example.com/fake.rss"
+      RSSParser.new("http://example.com/fake.rss").feed_url.should eql "http://example.com/fake.rss"
     end
+  end
+  
+  describe "parse" do
+    it "" do
+    end
+  end
+  
+  describe "parse_business_item" do
+  
+  end
+  
+  describe "parse_westminster_hall_item" do
+  end
+  
+  describe "parse_other_item" do
   end
 end
