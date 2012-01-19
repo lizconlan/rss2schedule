f0c718a5aca62ad0d2bdcd39f7c64d61e33262ca
Robert Brook <mail@robertbrook.com>
2012-01-19 15:34:50 +0000

    edits to wording of specs

--- spec/rss_parser_spec.rb
+++ spec/rss_parser_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 require 'models/item'
  
 describe RSSParser do 
-  describe "#new" do
+  describe "new" do
     before :each do
       RestClient.stub(:get).and_return("rss")
       @rssparser = RSSParser.new
     end
     
-    it "takes no parameters and returns a RSSParser object" do
+    it "creates an RSSParser" do
       @rssparser.should be_an_instance_of RSSParser
     end
     
-    it "returns the correct feed url" do
+    it "sets the feed URL to 'http://services.parliament.uk/calendar/all.rss' by default" do
       @rssparser.feed_url.should eql "http://services.parliament.uk/calendar/all.rss"
     end
     
-    it "accepts an alternate feed url" do
+    it "sets the feed URL when provided with one" do
       RSSParser.new("http://example.com/fake.rss").feed_url.should eql "http://example.com/fake.rss"
     end
   end
   
   describe "parse" do
-    describe "when given an rss feed with no parlyevent markup" do
+    describe "when given an RSS feed without ParlyCal markup" do
       before :each do
         RestClient.stub(:get).and_return(%Q|
 <rss xmlns:parlycal='http://services.parliament.uk/ns/calendar/feeds' version='2.0'>
@@ -71,35 +71,43 @@ describe RSSParser do
         @item.stub(:save)
       end
       
-      it "should call the parse_rss method" do
+      it "calls the parse_rss method" do
         @rssparser.should_receive(:parse_rss)
+        @rssparser.parse
+      end
+      
+      it "does not call the parse_parlyevent method" do
         @rssparser.should_not_receive(:parse_parlyevent)
         @rssparser.parse
       end
       
-      it "should correctly determine the house" do
+      it "sets the 'House' for the Item" do
         @item.should_receive(:house=).with("Commons")
         @rssparser.parse
       end
       
-      it "should work out the date" do
+      it "sets the 'Date' for the Item" do
         @item.should_receive(:date=).with("2012-02-24")
         @rssparser.parse
       end
       
-      it "should look for start and end times" do
+      it "sets the 'start time' for the Item" do
         @item.should_receive(:start_time=).with("9:30am")
+        @rssparser.parse
+      end
+      
+      it "sets the 'end time' for the Item" do
         @item.should_receive(:end_time=).with("11:00am")
         @rssparser.parse
       end
       
-      it "should work out the sponsor" do
+      it "sets the 'Sponsor' for the Item" do
         @item.should_receive(:sponsor=).with("Chi Onwurah")
         @rssparser.parse
       end
     end
 
-    describe "when given an rss feed with parlyevent markup" do
+    describe "when given an RSS feed with ParlyCal markup" do
       before :each do
         RestClient.stub(:get).and_return(%Q|
 <rss xmlns:parlycal='http://services.parliament.uk/ns/calendar/feeds' version='2.0'>
@@ -174,8 +182,12 @@ describe RSSParser do
         @item.stub(:save)
       end
       
-      it "should call the parse_parlyevent method" do
+      it "calls the parse_parlyevent method" do
         @rssparser.should_receive(:parse_parlyevent)
+        @rssparser.parse
+      end
+      
+      it "does not call the parse_rss method" do
         @rssparser.should_not_receive(:parse_rss)
         @rssparser.parse
       end
