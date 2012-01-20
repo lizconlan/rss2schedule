b382027f952a43c9ce49a2daca5dfb09fa68aa07
lizconlan <lizconlan@gmail.com>
2012-01-20 16:32:19 +0000

    renaming test vars

--- spec/rss_parser_spec.rb
+++ spec/rss_parser_spec.rb
@@ -45,10 +45,10 @@ describe RSSParser do
       
       describe "when parsing a House of Commons item" do
         before :each do
-          @rssitem = RssItem.new
+          @rssitem1 = RssItem.new
           @rssitem2 = RssItem.new
           @rssitem3 = RssItem.new
-          RssItem.should_receive(:new).and_return(@rssitem)
+          RssItem.should_receive(:new).and_return(@rssitem1)
           RssItem.should_receive(:new).and_return(@rssitem2)
           RssItem.should_receive(:new).and_return(@rssitem3)
         end
@@ -56,42 +56,42 @@ describe RSSParser do
         it "should set the event_id for the RssItem"
         
         it "sets the 'house' for the RssItem" do
-          @rssitem.should_receive(:house=).with("Commons")
+          @rssitem1.should_receive(:house=).with("Commons")
           @rssitem2.should_receive(:house=).with("Commons")
           @rssitem3.should_receive(:house=).with("Commons")
           @rssparser.parse
         end
         
         it "sets the 'chamber' for the RssItem" do
-          @rssitem.should_receive(:chamber=).at_least(1).times.with("Westminster Hall")
+          @rssitem1.should_receive(:chamber=).at_least(1).times.with("Westminster Hall")
           @rssitem2.should_receive(:chamber=).at_least(1).times.with("Main Chamber")
           @rssitem3.should_receive(:chamber=).at_least(1).times.with("Main Chamber")
           @rssparser.parse
         end
         
         it "should not set the 'committee' for the RssItem" do
-          @rssitem.should_not_receive(:committee=)
+          @rssitem1.should_not_receive(:committee=)
           @rssitem2.should_not_receive(:committee=)
           @rssitem3.should_not_receive(:committee=)
           @rssparser.parse
         end
         
         it "should set the 'subject' for the RssItem" do
-          @rssitem.should_receive(:subject=).with("House of Commons Westminster Hall - Chi Onwurah")
+          @rssitem1.should_receive(:subject=).with("House of Commons Westminster Hall - Chi Onwurah")
           @rssitem2.should_receive(:subject=).with("House of Commons Main Chamber - Business")
           @rssitem3.should_receive(:subject=).with("House of Commons Main Chamber - Legislation - Paul Maynard")
           @rssparser.parse
         end
         
         it "should set the 'inquiry' for the RssItem" do
-          @rssitem.should_receive(:inquiry=).with("House of Commons Westminster Hall - Chi Onwurah")
+          @rssitem1.should_receive(:inquiry=).with("House of Commons Westminster Hall - Chi Onwurah")
           @rssitem2.should_receive(:inquiry=).with("House of Commons Main Chamber - Business")
           @rssitem3.should_receive(:inquiry=).with("House of Commons Main Chamber - Legislation - Paul Maynard")
           @rssparser.parse
         end
         
         it "should set the 'note' for the RssItem" do
-          @rssitem.should_receive(:notes=).with("Friday 24 February 2012 - 9:30am - 11:00am <br /> Health inequalities in the North East today")
+          @rssitem1.should_receive(:notes=).with("Friday 24 February 2012 - 9:30am - 11:00am <br /> Health inequalities in the North East today")
           @rssitem2.should_receive(:notes=).with("Friday 24 February 2012 <br /> The House is not expected to sit today")
           @rssitem3.should_receive(:notes=).with("Friday 24 February 2012 <br /> Concessionary Bus Travel (Amendment) Bill - Second reading - Paul Maynard")
           @rssparser.parse
@@ -100,35 +100,35 @@ describe RSSParser do
         it "should set the 'location' for the RssItem"
         
         it "sets the 'date' for the RssItem" do
-          @rssitem.should_receive(:date=).with("2012-02-24")
+          @rssitem1.should_receive(:date=).with("2012-02-24")
           @rssitem2.should_receive(:date=).with("2012-02-24")
           @rssitem3.should_receive(:date=).with("2012-02-24")
           @rssparser.parse
         end
         
         it "sets the 'start time' for the RssItem (if there is one)" do
-          @rssitem.should_receive(:start_time=).with("9:30am")
+          @rssitem1.should_receive(:start_time=).with("9:30am")
           @rssitem2.should_not_receive(:start_time=)
           @rssitem3.should_not_receive(:start_time=)
           @rssparser.parse
         end
         
         it "sets the 'end time' for the RssItem (if there is one)" do
-          @rssitem.should_receive(:end_time=).with("11:00am")
+          @rssitem1.should_receive(:end_time=).with("11:00am")
           @rssitem2.should_not_receive(:end_time=)
           @rssitem3.should_not_receive(:end_time=)
           @rssparser.parse
         end
         
         it "sets the 'sponsor' for the RssItem (if there is one)" do
-          @rssitem.should_receive(:sponsor=).with("Chi Onwurah")
+          @rssitem1.should_receive(:sponsor=).with("Chi Onwurah")
           @rssitem2.should_not_receive(:sponsor=)
           @rssitem3.should_receive(:sponsor=).with("Paul Maynard")
           @rssparser.parse
         end
         
         it "sets the 'category' for the RssItem (if there is one)" do
-          @rssitem.should_not_receive(:category=)
+          @rssitem1.should_not_receive(:category=)
           @rssitem2.should_receive(:category=).with("Business")
           @rssitem3.should_receive(:category=).with("Legislation")
           @rssparser.parse
