b9bad2f7bc56670c857976152ffcf7ec1c2d8c43
lizconlan <lizconlan@gmail.com>
2012-01-26 18:15:11 +0000

    simplified Revision

--- models/item.rb
+++ models/item.rb
@@ -46,20 +46,5 @@ class Revision
   one :item
   
   key :date, Date
-  key :event_id, String
-  key :rss_id, String
-  key :source_file, String
-  
-  key :date, String
-  key :title, String
-  key :house, String
-  key :location, String
-  key :sponsor, String
-  key :start_time, String
-  key :end_time, String
-  
-  key :link, String
-  key :item_type, String
-  
-  key :notes, String
+  key :diff, Hash
 end
